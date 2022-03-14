2a190fb1ee9c87813431bce41f6e1358239b9311
Jeffrey Baucom <kishi@Jeffreys-MacBook-Pro.local>
2022-03-14 18:18:13 -0400

    remove excess landing page elements

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -30,23 +30,23 @@ export const IndexPageTemplate = ({
               <div className="column is-10 is-offset-1">
                 <div className="content">
                   <div className="content">
-                    <div className="tile">
+                    {/* <div className="tile">
                       <h1 className="title">{mainpitch.title}</h1>
-                    </div>
-                    <div className="tile">
+                    </div> */}
+                    {/* <div className="tile">
                       <h3 className="subtitle">{mainpitch.description}</h3>
-                    </div>
+                    </div> */}
                   </div>
-                  <div className="columns">
+                  {/* <div className="columns">
                     <div className="column is-12">
                       <h3 className="has-text-weight-semibold is-size-2">
                         {heading}
                       </h3>
                       <p>{description}</p>
                     </div>
-                  </div>
-                  <Features gridItems={intro.blurbs} />
-                  <div className="columns">
+                  </div> */}
+                  {/* <Features gridItems={intro.blurbs} /> */}
+                  {/* <div className="columns">
                     <div className="column is-12 has-text-centered">
                       <Link className="btn" to="/products">
                         See all products
@@ -63,7 +63,7 @@ export const IndexPageTemplate = ({
                         Read more
                       </Link>
                     </div>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             </div>
